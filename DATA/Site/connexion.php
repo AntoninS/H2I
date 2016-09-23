39ac734d5fa354fb24ea0b97113b774fc66a077f
CASTELLON ADRIEN p1503257 <adrien.castellon@etu.univ-lyon1.fr>
2016-09-23 09:22:56 +0200

    Revert "Revert "Site MAJ""
    This reverts commit eb596783a9fbf5dbc407de65bdd009eef9eb53f0.

---
+++ DATA/Site/connexion.php
@@ -0,0 +1,26 @@
+<!doctype html >
+<html >
+
+	<head>
+		<title>Connexion</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<link href="./style/connexion.css" media="all" rel="stylesheet " type="text/css" />
+		<script type="text/javascript" src="./scripts/jquery-1.12.3.js"></script>
+		
+		<meta name="language" content="fr" />
+
+	</head>
+
+		
+	<body>
+	
+	
+	<div id="boxLogin">
+	</div>
+	
+	
+	
+
+	</body>
+
+</html>

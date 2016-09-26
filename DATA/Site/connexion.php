a99b645e3507402a58683cf725f2b80aff8e994d
DemonazX <adrien.castellon@etu.univ-lyon1.fr>
2016-09-26 20:52:42 +0200

    Ajout des textes page connexion

--- DATA/Site/connexion.php
+++ DATA/Site/connexion.php
@@ -3,8 +3,9 @@
 <html >
 
 <head>
-	<title>H2I Connexion jhj</title>
+	<title>H2I Connexion</title>
 	<?php include('./includes/head.php'); ?>
+
 	<link href="./style/connexion.css" media="all" rel="stylesheet " type="text/css" />
 </head>
 
@@ -16,14 +17,18 @@
 	<div id="warpper" class="connexion">
 
 		<div id="boxLogin">
-			<div id="WarpperForm">
-			<form method ="post" >
-				<label>Identifiant</label>
-				<input name="identifiant" size="20" maxlength="20" type="text" required=""/><br>
-				<label>Mot de passe</label>
-				<input name="identifiant" size="20" maxlength="55" type="password" required=""/>
 
-				<button>SE CONNECTER</button>
+				<h1>Bienvenue sur H2I</h1>
+
+			<div id="WarpperForm">
+				<p>Pour vous connecter, veuillez remplir les champs utilisateurs et mot de passe suivants:</p>
+				<form method ="post" >
+					<label>Identifiant</label>
+					<input name="identifiant" size="20" maxlength="20" type="text" required="" placeholder="p150..."/><br>
+					<label>Mot de passe</label>
+					<input name="identifiant" size="20" maxlength="55" type="password" required=""/>
+
+					<button>SE CONNECTER</button>
 			</form>
 			</div>
 		</div>

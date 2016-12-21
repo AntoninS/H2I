7251d2d68f21cbb140c97d34256366798f1672e7
AntoninS <antonin.spontak@gmail.com>
2016-12-21 13:59:20 +0100

    CSS moins rustique (mais bugé) pour le formulaire d'ajout d'un tutorat
    Meilleur CSS pour le formulaire d'ajout d'un tutorat, néanmoins y'a des bugs : mauvais affichage des radio buttons

--- DATA/Site/Views/ajoutTutorat.php
+++ DATA/Site/Views/ajoutTutorat.php
@@ -1,42 +1,48 @@
 <?php
 		$title='H2I - Tutorat';
-		$pageCSS='tutorats';
+		$pageCSS='ajoutTutorat';
 
 		ob_start(); //mise en tempon début
 
     echo'
-      <form method="post" action=index.php?page=tutorats>
+			<div class="formulaireAjoutTutorat">
+				<div class="formulaireAjoutTutorat-entete">Renseignez vos informations</div>
 
-				<p>Pour quel module ? :
-					<select name="selectionModuleTutorat">';
-						foreach ($modulesDisponibles as $module)
-						{
-								$moduleSansEspace = str_replace(' ', '_', $module['nomModule']);		//On remplace les espaces par des '_' , sinon après dans la valeur option value les espaces passent pas
-								echo'<option value='.$moduleSansEspace.'> '.$module['nomModule'].'';
-						}
-						echo'
-					</select>
-				</p>
+	      <form method="post" action=index.php?page=tutorats>
 
-				<p>Quel jour ? : <input type="text" id="datepicker"  name="choixJourTutorat"/></p>
+					<p>
+						<label for="selectionModuleTutorat"><span>Pour quel module ? : <span class="required">*</span></span><select name="selectionModuleTutorat" class="champSelection"/></label>';
+							foreach ($modulesDisponibles as $module)
+							{
+									$moduleSansEspace = str_replace(' ', '_', $module['nomModule']);		//On remplace les espaces par des '_' , sinon après dans la valeur option value les espaces passent pas
+									echo'<option value='.$moduleSansEspace.'> '.$module['nomModule'].'';
+							}
+							echo'
+						</select>
+					</p>
 
-				<p>A quelle heure ? : <input id="timepicker" type="text" name ="choixHeureTutorat" class="time"/></p>
+					<p><label for="choixJourTutorat"><span>Quel jour ? : <span class="required">*</span></span><input type="text" id="datepicker"  name="choixJourTutorat" class="champInsertion"/></label></p>
 
-				<p>
-					Combien de temps ? (Durée du tutorat):
-					<input type="radio" name="dureeTutorat" value ="1"> 1h
-					<input type="radio" name="dureeTutorat" value ="2"> 2h
-				</p>
+					<p><label for="choixHeureTutorat"><span>A quelle heure ? : <span class="required">*</span></span><input id="timepicker" type="text" name ="choixHeureTutorat" class="champInsertion"/></label></p>
 
-				<p>
-					Ajouter un commentaire :
-					</br>
-					<textarea rows="4" cols="50">Ex : "J\'aimerais insister sur tel aspect de tel chapitre" ect...</textarea>
-				</p>
 
-        <input type="submit" value="Envoyer la demande">
-      </form>'
-    ;
+
+					<p>
+						<label for="commentaireTutorat"><span>Ajouter un commentaire : </span>
+						<textarea rows="3" cols="30" name="commentaireTutorat" class="champTextArea">Ex : "J\'aimerais insister sur tel aspect de tel chapitre" ect...</textarea>
+						</label>
+					</p>
+
+					<p>
+						<label><span>Durée souhaitée : <span class="required">*</span></span>
+						<input type="radio" name="dureeTutorat" value ="1"/> 1h
+						<input type="radio" name="dureeTutorat" value ="2"/> 2h
+						</label>
+					</p>
+					<label><span>&nbsp;</span><input type="submit" value="Envoyer la demande" /></label>
+	      </form>
+			</div>
+			';
 
     $content = ob_get_contents(); //récuprération du tampon dons une var
     ob_end_clean(); // vide le tampon

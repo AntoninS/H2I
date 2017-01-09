5ad82c31b13904739a25a09e79f03c94ee9199b9
AntoninS <antonin.spontak@gmail.com>
2017-01-09 12:18:44 +0100

    Ajout des icones manquantes

--- DATA/BD/tables_final.sql
+++ DATA/BD/tables_final.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.9
--- https://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Client :  localhost
--- Généré le :  Lun 09 Janvier 2017 à 08:53
--- Version du serveur :  5.5.52-MariaDB
--- Version de PHP :  5.4.16
+-- Client :  127.0.0.1
+-- Généré le :  Lun 09 Janvier 2017 à 11:17
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `p1505454`
+-- Base de données :  `h2i_iut`
 --
 
 -- --------------------------------------------------------
@@ -26,7 +26,7 @@ SET time_zone = "+00:00";
 -- Structure de la table `cours`
 --
 
-CREATE TABLE IF NOT EXISTS `cours` (
+CREATE TABLE `cours` (
   `coursID` int(11) NOT NULL,
   `nomCours` varchar(255) NOT NULL,
   `fileURL` varchar(255) NOT NULL,
@@ -48,8 +48,8 @@ INSERT INTO `cours` (`coursID`, `nomCours`, `fileURL`, `moduleIDC`, `auteurIDC`,
 -- Structure de la table `courstutorat`
 --
 
-CREATE TABLE IF NOT EXISTS `courstutorat` (
-  `id` int(30) unsigned NOT NULL,
+CREATE TABLE `courstutorat` (
+  `id` int(30) UNSIGNED NOT NULL,
   `nomModule` varchar(255) NOT NULL,
   `jour` date DEFAULT NULL,
   `heureDebut` time DEFAULT NULL,
@@ -61,18 +61,18 @@ CREATE TABLE IF NOT EXISTS `courstutorat` (
   `eleve4` int(11) DEFAULT NULL,
   `salle` varchar(8) DEFAULT NULL,
   `commentaireTutorat` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `courstutorat`
 --
 
 INSERT INTO `courstutorat` (`id`, `nomModule`, `jour`, `heureDebut`, `heureFin`, `tuteur`, `eleve1`, `eleve2`, `eleve3`, `eleve4`, `salle`, `commentaireTutorat`) VALUES
-(1, 'Programmation C', '2016-12-23', '08:00:00', '10:00:00', 3, 2, NULL, NULL, NULL, 'S13', 'Ex : "J''aimerais insister sur tel aspect de tel chapitre" ect...'),
-(2, 'BDA', '2016-12-19', '08:00:00', '09:00:00', 3, 2, NULL, NULL, NULL, 'S13', 'Ex : "J''aimerais insister sur tel aspect de tel chapitre" ect...'),
-(3, 'BDA', '2016-12-19', '09:00:00', '10:00:00', 3, 2, NULL, NULL, NULL, 'S13', 'Ex : "J''aimerais insister sur tel aspect de tel chapitre" ect...'),
-(4, 'BDA', '2016-12-20', '08:00:00', '09:00:00', 3, 2, NULL, NULL, NULL, 'S13', 'Ex : "J''aimerais insister sur tel aspect de tel chapitre" ect...'),
-(5, 'Programmation C', '2016-12-28', '11:00:00', '13:00:00', 3, 2, NULL, NULL, NULL, 'S13', 'Ex : "J''aimerais insister sur tel aspect de tel chapitre" ect...');
+(1, 'Programmation C', '2016-12-23', '08:00:00', '10:00:00', 3, 2, NULL, NULL, NULL, 'S13', 'Ex : "J\'aimerais insister sur tel aspect de tel chapitre" ect...'),
+(2, 'BDA', '2016-12-19', '08:00:00', '09:00:00', 3, 2, NULL, NULL, NULL, 'S13', 'Ex : "J\'aimerais insister sur tel aspect de tel chapitre" ect...'),
+(3, 'BDA', '2016-12-19', '09:00:00', '10:00:00', 3, 2, NULL, NULL, NULL, 'S13', 'Ex : "J\'aimerais insister sur tel aspect de tel chapitre" ect...'),
+(4, 'BDA', '2016-12-20', '08:00:00', '09:00:00', 3, 2, NULL, NULL, NULL, 'S13', 'Ex : "J\'aimerais insister sur tel aspect de tel chapitre" ect...'),
+(5, 'Programmation C', '2016-12-28', '11:00:00', '13:00:00', 3, 2, NULL, NULL, NULL, 'S13', 'Ex : "J\'aimerais insister sur tel aspect de tel chapitre" ect...');
 
 -- --------------------------------------------------------
 
@@ -80,11 +80,11 @@ INSERT INTO `courstutorat` (`id`, `nomModule`, `jour`, `heureDebut`, `heureFin`,
 -- Structure de la table `groupe`
 --
 
-CREATE TABLE IF NOT EXISTS `groupe` (
+CREATE TABLE `groupe` (
   `GroupeID` int(11) NOT NULL,
   `NomGroupe` varchar(2) NOT NULL,
   `Semestre` int(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `groupe`
@@ -102,7 +102,7 @@ INSERT INTO `groupe` (`GroupeID`, `NomGroupe`, `Semestre`) VALUES
 -- Structure de la table `message`
 --
 
-CREATE TABLE IF NOT EXISTS `message` (
+CREATE TABLE `message` (
   `messageID` int(11) NOT NULL,
   `auteurID` int(11) NOT NULL,
   `sujetID` int(11) NOT NULL,
@@ -112,7 +112,7 @@ CREATE TABLE IF NOT EXISTS `message` (
   `premierMessage` tinyint(1) NOT NULL,
   `modification` datetime DEFAULT NULL,
   `pseudo` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=280 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `message`
@@ -133,40 +133,41 @@ INSERT INTO `message` (`messageID`, `auteurID`, `sujetID`, `contenu`, `dateMessa
 -- Structure de la table `module`
 --
 
-CREATE TABLE IF NOT EXISTS `module` (
+CREATE TABLE `module` (
   `moduleID` int(11) NOT NULL,
   `nomModule` varchar(255) NOT NULL,
   `semestre` int(11) NOT NULL,
   `Coefficient` float NOT NULL,
   `tuteur` int(11) DEFAULT NULL,
-  `nbEpingle` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+  `nbEpingle` int(11) NOT NULL DEFAULT '0',
+  `image` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `module`
 --
 
-INSERT INTO `module` (`moduleID`, `nomModule`, `semestre`, `Coefficient`, `tuteur`, `nbEpingle`) VALUES
-(3, 'Probabilités et Statistiques', 3, 2.5, NULL, 0),
-(4, 'Communication ', 1, 0, NULL, 1),
-(5, 'Programmation C', 1, 0, 3, 0),
-(6, 'Programmation Java', 2, 0, NULL, 0),
-(7, 'Communication', 2, 0, NULL, 0),
-(8, 'Communication', 3, 1.5, NULL, 0),
-(9, 'Communication', 4, 0, NULL, 0),
-(10, 'BDA', 3, 1.5, 3, 0),
-(11, 'Algo avancé', 4, 0, NULL, 0),
-(12, 'SE cours ', 3, 1.5, NULL, 0),
-(13, 'SE TP', 3, 1, NULL, 0),
-(14, 'Réseau', 3, 1.5, NULL, 0),
-(15, 'Algo avancée', 3, 1.5, NULL, 0),
-(16, 'PHP', 3, 1.5, NULL, 0),
-(17, 'CPOA', 3, 2.5, NULL, 0),
-(18, 'Modélisation mathématiques', 3, 1.5, NULL, 0),
-(19, 'TIC Droit', 3, 1.5, NULL, 0),
-(20, 'GSI 1', 3, 1.3, NULL, 0),
-(21, 'GSI 2', 3, 1.2, NULL, 0),
-(22, 'Anglais', 0, 2.5, NULL, 3);
+INSERT INTO `module` (`moduleID`, `nomModule`, `semestre`, `Coefficient`, `tuteur`, `nbEpingle`, `image`) VALUES
+(3, 'Probabilités et Statistiques', 3, 2.5, NULL, 0, 'iconmaths'),
+(4, 'Communication ', 1, 0, NULL, 1, 'iconcom'),
+(5, 'Programmation C', 1, 0, 3, 0, 'iconc'),
+(6, 'Programmation Java', 2, 0, NULL, 0, 'iconjava'),
+(7, 'Communication', 2, 0, NULL, 0, 'iconcom'),
+(8, 'Communication', 3, 1.5, NULL, 0, 'iconcom'),
+(9, 'Communication', 4, 0, NULL, 0, 'iconcom'),
+(10, 'BDA', 3, 1.5, 3, 0, 'iconsql'),
+(11, 'Algo avancé', 4, 0, NULL, 0, 'iconalgo'),
+(12, 'SE cours ', 3, 1.5, NULL, 0, 'iconwindows'),
+(13, 'SE TP', 3, 1, NULL, 0, 'iconlinux'),
+(14, 'Réseau', 3, 1.5, NULL, 0, 'iconreseau'),
+(15, 'Algo avancée', 3, 1.5, NULL, 0, 'iconalgo'),
+(16, 'PHP', 3, 1.5, NULL, 0, 'iconphp'),
+(17, 'CPOA', 3, 2.5, NULL, 0, 'iconcpoa'),
+(18, 'Modélisation mathématiques', 3, 1.5, NULL, 0, 'iconmaths'),
+(19, 'TIC Droit', 3, 1.5, NULL, 0, 'icondroit'),
+(20, 'GSI 1', 3, 1.3, NULL, 0, 'icongsi'),
+(21, 'GSI 2', 3, 1.2, NULL, 0, 'icongsi'),
+(22, 'Anglais', 0, 2.5, NULL, 3, 'iconenglish');
 
 -- --------------------------------------------------------
 
@@ -174,15 +175,15 @@ INSERT INTO `module` (`moduleID`, `nomModule`, `semestre`, `Coefficient`, `tuteu
 -- Structure de la table `planningtutorat`
 --
 
-CREATE TABLE IF NOT EXISTS `planningtutorat` (
+CREATE TABLE `planningtutorat` (
   `annee` int(4) NOT NULL,
-  `numeroSemaine` int(2) unsigned NOT NULL,
+  `numeroSemaine` int(2) UNSIGNED NOT NULL,
   `heurePlanning` int(4) NOT NULL,
-  `lundi` int(30) unsigned DEFAULT NULL,
-  `mardi` int(30) unsigned DEFAULT NULL,
-  `mercredi` int(30) unsigned DEFAULT NULL,
-  `jeudi` int(30) unsigned DEFAULT NULL,
-  `vendredi` int(30) unsigned DEFAULT NULL
+  `lundi` int(30) UNSIGNED DEFAULT NULL,
+  `mardi` int(30) UNSIGNED DEFAULT NULL,
+  `mercredi` int(30) UNSIGNED DEFAULT NULL,
+  `jeudi` int(30) UNSIGNED DEFAULT NULL,
+  `vendredi` int(30) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -237,7 +238,19 @@ INSERT INTO `planningtutorat` (`annee`, `numeroSemaine`, `heurePlanning`, `lundi
 (2017, 2, 16, NULL, NULL, NULL, NULL, NULL),
 (2017, 2, 17, NULL, NULL, NULL, NULL, NULL),
 (2017, 2, 18, NULL, NULL, NULL, NULL, NULL),
-(2017, 2, 19, NULL, NULL, NULL, NULL, NULL);
+(2017, 2, 19, NULL, NULL, NULL, NULL, NULL),
+(2017, 3, 8, NULL, NULL, NULL, NULL, NULL),
+(2017, 3, 9, NULL, NULL, NULL, NULL, NULL),
+(2017, 3, 10, NULL, NULL, NULL, NULL, NULL),
+(2017, 3, 11, NULL, NULL, NULL, NULL, NULL),
+(2017, 3, 12, NULL, NULL, NULL, NULL, NULL),
+(2017, 3, 13, NULL, NULL, NULL, NULL, NULL),
+(2017, 3, 14, NULL, NULL, NULL, NULL, NULL),
+(2017, 3, 15, NULL, NULL, NULL, NULL, NULL),
+(2017, 3, 16, NULL, NULL, NULL, NULL, NULL),
+(2017, 3, 17, NULL, NULL, NULL, NULL, NULL),
+(2017, 3, 18, NULL, NULL, NULL, NULL, NULL),
+(2017, 3, 19, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -245,7 +258,7 @@ INSERT INTO `planningtutorat` (`annee`, `numeroSemaine`, `heurePlanning`, `lundi
 -- Structure de la table `sujet`
 --
 
-CREATE TABLE IF NOT EXISTS `sujet` (
+CREATE TABLE `sujet` (
   `sujetID` int(11) NOT NULL,
   `nom` varchar(255) NOT NULL,
   `auteurID` int(11) NOT NULL,
@@ -261,7 +274,7 @@ CREATE TABLE IF NOT EXISTS `sujet` (
   `dernierMessage` int(11) DEFAULT NULL,
   `pseudo` varchar(255) DEFAULT NULL,
   `dateDernierChangement` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=153 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `sujet`
@@ -278,7 +291,7 @@ INSERT INTO `sujet` (`sujetID`, `nom`, `auteurID`, `moduleID`, `message`, `dateS
 -- Structure de la table `utilisateurs`
 --
 
-CREATE TABLE IF NOT EXISTS `utilisateurs` (
+CREATE TABLE `utilisateurs` (
   `utilisateurID` int(11) NOT NULL,
   `identifiant` varchar(255) NOT NULL,
   `motDePasse` varchar(255) NOT NULL,
@@ -291,7 +304,7 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `statut` varchar(255) DEFAULT NULL,
   `avatar` varchar(123) NOT NULL DEFAULT 'USR',
   `public` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `utilisateurs`
@@ -379,32 +392,32 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `courstutorat`
 --
 ALTER TABLE `courstutorat`
-  MODIFY `id` int(30) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(30) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT pour la table `groupe`
 --
 ALTER TABLE `groupe`
-  MODIFY `GroupeID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `GroupeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT pour la table `message`
 --
 ALTER TABLE `message`
-  MODIFY `messageID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=280;
+  MODIFY `messageID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=280;
 --
 -- AUTO_INCREMENT pour la table `module`
 --
 ALTER TABLE `module`
-  MODIFY `moduleID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+  MODIFY `moduleID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 --
 -- AUTO_INCREMENT pour la table `sujet`
 --
 ALTER TABLE `sujet`
-  MODIFY `sujetID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=153;
+  MODIFY `sujetID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=153;
 --
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `utilisateurID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `utilisateurID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- Contraintes pour les tables exportées
 --
@@ -413,11 +426,11 @@ ALTER TABLE `utilisateurs`
 -- Contraintes pour la table `courstutorat`
 --
 ALTER TABLE `courstutorat`
-  ADD CONSTRAINT `fk_tuteur_utilisateurs` FOREIGN KEY (`tuteur`) REFERENCES `utilisateurs` (`utilisateurID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_eleve1_utilisateurs` FOREIGN KEY (`eleve1`) REFERENCES `utilisateurs` (`utilisateurID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_eleve2_utilisateurs` FOREIGN KEY (`eleve2`) REFERENCES `utilisateurs` (`utilisateurID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_eleve3_utilisateurs` FOREIGN KEY (`eleve3`) REFERENCES `utilisateurs` (`utilisateurID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_eleve4_utilisateurs` FOREIGN KEY (`eleve4`) REFERENCES `utilisateurs` (`utilisateurID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_eleve4_utilisateurs` FOREIGN KEY (`eleve4`) REFERENCES `utilisateurs` (`utilisateurID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_tuteur_utilisateurs` FOREIGN KEY (`tuteur`) REFERENCES `utilisateurs` (`utilisateurID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Contraintes pour la table `groupe`
@@ -435,11 +448,11 @@ ALTER TABLE `module`
 -- Contraintes pour la table `planningtutorat`
 --
 ALTER TABLE `planningtutorat`
-  ADD CONSTRAINT `fk_vendredi_courstutorat` FOREIGN KEY (`vendredi`) REFERENCES `courstutorat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_jeudi_courstutorat` FOREIGN KEY (`jeudi`) REFERENCES `courstutorat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_lundi_courstutorat` FOREIGN KEY (`lundi`) REFERENCES `courstutorat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_mardi_courstutorat` FOREIGN KEY (`mardi`) REFERENCES `courstutorat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_mercredi_courstutorat` FOREIGN KEY (`mercredi`) REFERENCES `courstutorat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_mercredi_courstutorat` FOREIGN KEY (`mercredi`) REFERENCES `courstutorat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_vendredi_courstutorat` FOREIGN KEY (`vendredi`) REFERENCES `courstutorat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

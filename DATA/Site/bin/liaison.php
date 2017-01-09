7db3cbdc9e6d2b2e68164f4fb0020a804dc29aeb
AntoninS <antonin.spontak@gmail.com>
2017-01-09 11:38:11 +0100

    Fix pb de connexion, ajout fichier .sql tables finales

---
+++ DATA/Site/bin/liaison.php
@@ -0,0 +1,13 @@
+<?php /*
+$host ="db4free.net:3306";
+$user ="admin_h2i";
+$password="salut4dab";
+$nombase="h2i_iut";
+*/
+
+$host ="localhost";
+$user ="root";
+$password="";
+$nombase="h2i_iut";
+
+?>

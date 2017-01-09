b7099c7db28b203fdc4f639507be199574fe6231
BLANC-PATTIN LAURENT p1505454 <laurent.blanc-pattin@etu.univ-lyon1.fr>
2017-01-09 18:47:54 +0100

    Revert "Merge pull request #4 from AntoninS/Profil"
    This reverts commit bf6668a68344374e02eeff2c7f9f309806f142c7, reversing
    changes made to 5ad82c31b13904739a25a09e79f03c94ee9199b9.

--- DATA/Site/Model/ModulesManager.php
+++ DATA/Site/Model/ModulesManager.php
@@ -2,6 +2,12 @@
 		require_once ("Model.php");
 	 	class ModulesManager extends Model
 		{
+			public function getModul()
+			{
+				$req=$this->executerRequete('SELECT * FROM module');
+				$result=$req->fetchALL(PDO::FETCH_ASSOC);
+				return $result;
+			}
 			
 			public function getModules($semestreID)
 			{
@@ -9,14 +15,14 @@
 				$result=$req->fetchALL(PDO::FETCH_ASSOC);
 				return $result;
 			}
-			
+
 			public function getModule($idModule)
 			{
 				$req=$this->executerRequete('SELECT * FROM module WHERE moduleID=?', array($idModule));
 				$result=$req->fetch(PDO::FETCH_ASSOC);
 				return $result;
 			}
-			
+
 			public function getNom($moduleID)
 			{
 				$req=$this->executerRequete('SELECT module.nomModule FROM module WHERE moduleID=?', array($moduleID));

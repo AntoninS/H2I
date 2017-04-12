b8789d25272d71e648d493ebda4a29d928150170
laurentbp <laurent.blanc-pattin@etu.univ-lyon1.fr>
2017-04-11 23:13:06 -0400

    Améliorations ergonomiques, édition d'annonce

--- DATA/Site/Views/groupe/annonce.php
+++ DATA/Site/Views/groupe/annonce.php
@@ -68,26 +68,28 @@
 					
 			<div id="bloc">
 								
-					<div id="annonces">';
+					<div id="annonces">
+					
+						<div id="details_annonce">';
 			
-						if(isset($annonceEdition))
-						{
-							echo '
-							<form method="post" action="index.php?page=groupe&actionGroupe=modif_annonce&ida='.$annonce['annonceID'].'">
-								<p class="titre_annonce"><a href="index.php?page=groupe&actionGroupe=annonce&ida='.$annonce['annonceID'].'">'.strtoupper($annonce['nom']).'</a></p>
-								<p class="type_annonce"><a href="index.php?page=groupe&channel='.$annonce['type'].'" class="type_link_annonce">↳ '.$annonce['type'].'</a></p>
-								<img class="avatar_annonce" src="uploads/avatar/'.$annonce['avatar'].'" alt="Avatar">
-								<p class="auteur_annonce"><a href="index.php?page=monCompte&compte='.$annonce['auteurID'].'">'.$annonce['prenomAuteur'].' '.$annonce['nomAuteur'].'</a></p>
-								<p class="date_annonce">'.$annonce['dateAnnonce'].'</p>
-								<p><textarea name="message" id="message">'.$annonce['message'].'</textarea></p>
-								<input type="submit" value="Publier">
-							</form>';
-            			}
-						
-						else 
-						{
-							echo '<div id="options">';
-									
+							if(isset($annonceEdition))
+							{
+								echo '
+								<form method="post" action="index.php?page=groupe&actionGroupe=modif_annonce&ida='.$annonce['annonceID'].'">
+									<p class="titre_annonce"><a href="index.php?page=groupe&actionGroupe=annonce&ida='.$annonce['annonceID'].'">'.strtoupper($annonce['nom']).'</a></p>
+									<p class="type_annonce"><a href="index.php?page=groupe&channel='.$annonce['type'].'" class="type_link_annonce">↳ '.$annonce['type'].'</a></p>
+									<img class="avatar_annonce" src="uploads/avatar/'.$annonce['avatar'].'" alt="Avatar">
+									<p class="auteur_annonce"><a href="index.php?page=monCompte&compte='.$annonce['auteurID'].'">'.$annonce['prenomAuteur'].' '.$annonce['nomAuteur'].'</a></p>
+									<p class="date_annonce">'.$annonce['dateAnnonce'].'</p>
+									<p><textarea name="message" id="message_comment">'.$annonce['message'].'</textarea></p>
+									<input type="submit" value="Publier">
+								</form>';
+	            			}
+							
+							else 
+							{
+								echo '<div id="options">';
+										
 								if($annonce['auteurID']==$utilisateurID || $groupe['responsable']==$utilisateurID)
 								{
 									echo '<p><a href="index.php?page=groupe&actionGroupe=supprimer&ida='.$annonce['annonceID'].'" id="delete">Supprimer</a></p>';
@@ -103,78 +105,84 @@
 								{
 									echo '<p><a href="index.php?page=groupe&actionGroupe=desepingler&ida='.$annonce['annonceID'].'" id="delete">Desépingler</a></p>';
 								}
-								
-							echo '</div>
 									
-							<div id="details_annonce">
-							<p class="titre_annonce"><a href="index.php?page=groupe&actionGroupe=annonce&ida='.$annonce['annonceID'].'">'.strtoupper($annonce['nom']).'</a></p>
-							<p class="type_annonce"><a href="index.php?page=groupe&channel='.$annonce['type'].'" class="type_link_annonce">↳ '.$annonce['type'].'</a></p>
-							<img class="avatar_annonce" src="uploads/avatar/'.$annonce['avatar'].'" alt="Avatar">
-							<p class="auteur_annonce"><a href="index.php?page=monCompte&compte='.$annonce['auteurID'].'">'.$annonce['prenomAuteur'].' '.$annonce['nomAuteur'].'</a></p>
-							<p class="date_annonce">'.$annonce['dateAnnonce'].'</p>
-							<p class="contenu_annonce">'.$annonce['message'].'</p>
-							<p id="comments_annonce">Commentaires - '.$annonce['nbComment'].'</p>
+								echo '</div>
+									
+								<p class="titre_annonce"><a href="index.php?page=groupe&actionGroupe=annonce&ida='.$annonce['annonceID'].'">'.strtoupper($annonce['nom']).'</a></p>
+								<p class="type_annonce"><a href="index.php?page=groupe&channel='.$annonce['type'].'" class="type_link_annonce">↳ '.$annonce['type'].'</a></p>
+								<img class="avatar_annonce" src="uploads/avatar/'.$annonce['avatar'].'" alt="Avatar">
+								<p class="auteur_annonce"><a href="index.php?page=monCompte&compte='.$annonce['auteurID'].'">'.$annonce['prenomAuteur'].' '.$annonce['nomAuteur'].'</a></p>
+								<p class="date_annonce">'.$annonce['dateAnnonce'].'</p>
+								<p class="contenu_annonce">'.$annonce['message'].'</p>';
 								
-							<div id="commentaires">
-							
-								<table>
-										
-									<div id="publi_commentaire">
-										<form method="post" action="index.php?page=groupe&actionGroupe=ajout_commentaire&ida='.$annonce['annonceID'].'">
-											<img class="avatar_comment" src="uploads/avatar/'.$utilisateur['avatar'].'" alt="Avatar">
-											<textarea name="message" id="message_comment" placeholder="Ajouter un commentaire"></textarea>		
-											<input id="publi_comment" type="submit" value="Publier">
-										</form>
-									</div>';
-					
-									foreach($comments as $comment)
-									{
-										if(isset($commentaireEdition) && $comment['commentaireID']==$commentaireEdition)
+								if(isset($annonce['modification']))
+								{
+									echo '<p class="date_modif_annonce">Modifié le '.$annonce['modification'].'</p>';
+								}
+								
+								echo '<p id="comments_annonce">Commentaires - '.$annonce['nbComment'].'</p>
+									
+								<div id="commentaires">
+								
+									<table>
+											
+										<div id="publi_commentaire">
+											<form method="post" action="index.php?page=groupe&actionGroupe=ajout_commentaire&ida='.$annonce['annonceID'].'">
+												<img class="avatar_comment" src="uploads/avatar/'.$utilisateur['avatar'].'" alt="Avatar">
+												<textarea name="message" id="message_comment" placeholder="Ajouter un commentaire"></textarea>		
+												<input id="publi_comment" type="submit" value="Publier">
+											</form>
+										</div>';
+						
+										foreach($comments as $comment)
 										{
-											echo '
-											<form method="post" action="index.php?page=groupe&actionGroupe=modif_comment&idc='.$comment['commentaireID'].'">
-												<tr>
+											if(isset($commentaireEdition) && $comment['commentaireID']==$commentaireEdition)
+											{
+												echo '
+												<form method="post" action="index.php?page=groupe&actionGroupe=modif_comment&idc='.$comment['commentaireID'].'">
+													<tr>
+														<td>
+															<p class="auteur_comment">
+																<a href="index.php?page=monCompte&compte='.$comment['auteurID'].'">'.$comment['prenom'].' '.$comment['nom'].'</a>
+																<span class="date_comment"> le '.$comment['dateCommentaire'].'</span>
+															</p>
+															<img class="avatar_comment" src="uploads/avatar/'.$comment['avatar'].'" alt="Avatar">
+															<textarea name="message" id="message_comment">'.$comment['message'].'</textarea>
+															<input id="publi_comment" type="submit" value="Publier">
+														</td>
+													</tr>
+												</form>';
+											}
+											else
+											{
+												echo '<tr>
 													<td>
 														<p class="auteur_comment">
 															<a href="index.php?page=monCompte&compte='.$comment['auteurID'].'">'.$comment['prenom'].' '.$comment['nom'].'</a>
 															<span class="date_comment"> le '.$comment['dateCommentaire'].'</span>
 														</p>
 														<img class="avatar_comment" src="uploads/avatar/'.$comment['avatar'].'" alt="Avatar">
-														<textarea name="message" id="message_comment">'.$comment['message'].'</textarea>
-														<input id="publi_comment" type="submit" value="Publier">
+														<p class="comment_message">'.$comment['message'].'</p>
+														<a class="comment_options" href="index.php?page=groupe&actionGroupe=suppr_comment&idc='.$comment['commentaireID'].'">Supprimer</a>
+														<a class="comment_options" href="index.php?page=groupe&actionGroupe=edit_comment&idc='.$comment['commentaireID'].'">Éditer</a>
+														<a href="index.php?page=groupe&actionGroupe=thumb_up&idc='.$comment['commentaireID'].'"><img class="thumb" src="media/images/thumb.png"></a> '.$comment['nbPouce'].'
 													</td>
-												</tr>
-											</form>';
+												</tr>';
+											}
+											
 										}
-										else
-										{
-											echo '<tr>
-												<td>
-													<p class="auteur_comment">
-														<a href="index.php?page=monCompte&compte='.$comment['auteurID'].'">'.$comment['prenom'].' '.$comment['nom'].'</a>
-														<span class="date_comment"> le '.$comment['dateCommentaire'].'</span>
-													</p>
-													<img class="avatar_comment" src="uploads/avatar/'.$comment['avatar'].'" alt="Avatar">
-													<p class="comment_message">'.$comment['message'].'</p>
-													<a class="comment_options" href="index.php?page=groupe&actionGroupe=suppr_comment&idc='.$comment['commentaireID'].'">Supprimer</a>
-													<a class="comment_options" href="index.php?page=groupe&actionGroupe=edit_comment&idc='.$comment['commentaireID'].'">Éditer</a>
-													<a href="index.php?page=groupe&actionGroupe=thumb_up&idc='.$comment['commentaireID'].'"><img class="thumb" src="media/images/thumb.png"></a> '.$comment['nbPouce'].'
-												</td>
-											</tr>';
-										}
-										
-									}
-					
-								echo '</table>
-									
-							</div>';
-						}
 						
-					echo '</div>
+									echo '</table>
+										
+								</div>';
+					
+							}
 							
+						echo '</div>
+							
+					</div>
+									
 				</div>
-								
-			</div>
 			
 			<div id="news">
 								
